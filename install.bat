aa38a7626872c5349bcb7ebab9b7e678bcf4db0c
ZippyMagic <zippymagician1@gmail.com>
2017-05-25 14:16:32 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,7 @@
 @echo off
+echo "Creating Directory..."
 mkdir C:\iSecure
+echo "Downloading..."
 bitsadmin.exe /transfer "Download" https://raw.githubusercontent.com/ZippyMagic/iSecure/master/ChatClient.java C:\iSecure\ChatClient.java
+echo "Done!"
+exit
